395c23976b8687a92d676e47e8135d634261517e
aalifard <a03n55a@gmail.com>
2024-11-19 13:31:03 +0300

    added skeleton to patient page

--- frontend/src/pages/Dashboards/Patient/patientHome.jsx
+++ frontend/src/pages/Dashboards/Patient/patientHome.jsx
@@ -1,9 +1,47 @@
-function PatientHome () {
-    return(
-        <>
+import StatBox from "@/components/statBox";
 
-        </>
-    );
+function PatientHome() {
+  return (
+    <>
+      <div className="p-6 grid grid-cols-12 md:grid-cols-12 lg:grid-cols-8 gap-6 bg-even-darker-background h-full">
+        {/* Row 1 - Stat Boxes */}
+        <div className="col-span-12 lg:col-span-6">
+          <div className="grid grid-cols-12 md:grid-cols-12 lg:grid-cols-8 gap-6">
+            <div className="col-span-12 md:col-span-6 lg:col-span-2">
+              <StatBox title="Todays Patients" number="34" />
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-2">
+              <StatBox title="Appt. Done" number="10" />
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-2">
+              <StatBox title="New Patients" number="15" />
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-2">
+              <StatBox title="Returning Patients" number="20" />
+            </div>
+          </div>
+
+          {/* Main Content */}
+          <div className="p-4 bg-background rounded-lg shadow-md mb-6 text-foreground mt-6">
+            Main Content
+          </div>
+        </div>
+
+        {/* Sidebar */}
+        <div className="col-span-12 lg:col-span-2">
+          <div className="p-4 bg-background rounded-lg shadow-md mb-6 text-foreground min-h-36">
+            Smaller Box
+          </div>
+          <div className="p-4 bg-background rounded-lg shadow-md mb-6 text-foreground min-h-36">
+            Smaller Box
+          </div>
+          <div className="p-4 bg-background rounded-lg shadow-md flex-grow text-foreground">
+            Longer Box
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default PatientHome
+export default PatientHome;
